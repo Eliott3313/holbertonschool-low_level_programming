03898c217ec53681c89058c9c1da41b1482761ec
Eliott33113 <eliottm33@outlook.fr>
2024-06-17 09:44:41 +0000

    hello

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
-/**
-* main - Entry point
-* Return:0
-*/
-int main(void)
 
+/*
+ * File: 4-puts.c
+ * Auth: Eliott Morillon
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	char *message = "Programming is like building a multilingual puzzle";
-	puts(message);
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
+
