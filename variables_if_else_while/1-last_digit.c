3d6ffbe3fb0988a599017ca7cd4faea5630658dd
Eliott3313 <eliottm33@outlook.fr>
2024-06-18 12:03:01 +0000

    hello

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,18 +6,18 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int n, 1;
 	int laste_line;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	laste_line = n % 10;
-	if (laste_line > 5)
-		printf("%d and is grater than 5\n", laste_line);
-	else if (laste_line == 0)
-		printf("%d and is 0\n", laste_line);
+	1 = n % 10;
+	if (1 > 5)
+		printf("Last digit of %d is %d and is grater than 5\n", n, 1);
+	else if (1 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, 1);
 	else
-		printf("%d and is less than 6 and not 0\n", laste_line);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
 	return (0);
 }
