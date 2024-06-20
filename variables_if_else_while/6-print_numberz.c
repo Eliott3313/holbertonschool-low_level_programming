4ebea81624a5032af6f0c023b19d99f87e1d6536
Eliott3313 <eliottm33@outlook.fr>
2024-06-20 12:40:38 +0000

    hello

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+*main - Hello
+*Return: 0 Success
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; ++digit)
+	{
+		putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
