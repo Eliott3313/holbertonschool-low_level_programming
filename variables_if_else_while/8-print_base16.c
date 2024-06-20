dcf131899d166f95e4b6d3ec5c494376b03baf60
Eliott3313 <eliottm33@outlook.fr>
2024-06-20 13:06:29 +0000

    hello

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Hello
+*Return: 0 Success
+*/
+
+int main(void)
+{
+	int num1;
+	char letter;
+
+	for (num1 = 0 ; num1 <= 10 ; num1++)
+	{
+		putchar(num1);
+	}
+	for (letter = 'a' ; letter <= 'f' ; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
