9fa80043e04cf141771cef75411b4bb568269f54
Eliott3313 <eliottm33@outlook.fr>
2024-06-21 08:01:09 +0000

    hello

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 *main - Hello
 *Return: 0 Success
@@ -8,7 +7,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 
 	return (0);
 }
