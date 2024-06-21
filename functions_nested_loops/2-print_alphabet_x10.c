9a1689df68b004440853510f95915f93c598bf9d
Eliott3313 <eliottm33@outlook.fr>
2024-06-21 13:54:08 +0000

    hello

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_alphabet - Hello
+*print_alphabet - print alphabet 10 x
 *Return: 0 Success
 */
 
@@ -15,8 +15,11 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
+			if (letter == 'z')
+			{
+				_putchar('\n');
+			}
 		}
 	}
-	_putchar('\n');
 
 }
