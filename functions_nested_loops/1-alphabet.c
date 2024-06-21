3d0454ee2d283a54ab708ac86c170d6e09eeff78
Eliott3313 <eliottm33@outlook.fr>
2024-06-21 10:39:07 +0000

    hello

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - Hello
+*Return: 0 Success
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+	return (0);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef main_c
-#define main_c
+#ifndef main_h
+#define main_h
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
