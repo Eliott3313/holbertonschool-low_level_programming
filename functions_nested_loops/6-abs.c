9d77264310dde9a0fd9b090d839ada67badfb4d5
Eliott3313 <eliottm33@outlook.fr>
2024-06-22 16:46:33 +0000

    hello

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_abs - Hello
+*Return: num Success
+*@num: number return
+*/
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	return (num);
+}
