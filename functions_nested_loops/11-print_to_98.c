0c5ae9b5a15f880cb280f87b3060ed0bbe8c04e0
Eliott3313 <eliottm33@outlook.fr>
2024-06-23 21:34:40 +0000

    hello

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,21 @@
 
 void print_to_98(int n)
 {
-	int i;
 
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (i = n; i < 98; i++)
+		while (n > 98)
 		{
-			printf("%d, ", i);
+			printf("%d, ", n--);
 		}
-		printf("98\n");
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
 	}
 }
