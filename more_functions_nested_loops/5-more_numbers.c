7cb2239b7c014a79e26cc4840ff4ca5bb0cd5d4a
Eliott3313 <eliottm33@outlook.fr>
2024-06-25 08:48:08 +0000

    hello

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,30 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *more_numbers - Hello
-*
-*
+*Return: none
 */
 
 void more_numbers(void)
 {
-	int num;
-	int max;
+	int num, max;
 
-	char digit[] = "0123456789";
-
-	for (max = 0; max <= 9; max++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (max = 0; max <= 14; max++)
 		{
-			if (num < 10 || num == 14)
-			{
-				if (num >= 10)
-				{
-					_putchar(digit[num / 10]);
-				}
-				_putchar(digit[num % 10]);
-			}
+			if (max >= 10 && max <= 14)
+			_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
 		}
 	_putchar('\n');
 	}
