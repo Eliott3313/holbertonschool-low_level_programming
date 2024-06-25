e88e18f9c908b8da838581cdafc6a977ce533e09
Eliott3313 <eliottm33@outlook.fr>
2024-06-25 21:13:14 +0000

    hello

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Hello
+*Return: 0 Success
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i ", num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
