a09b4cd94c07bb0d09a27eef7abe9fba1921e911
Eliott3313 <eliottm33@outlook.fr>
2024-06-25 15:21:58 +0000

    hello

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,18 @@
 
 void print_line(int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < 0)
+	if (n > 0)
 	{
-		if (n > 0)
+		for (i = 0 ; i < n ; i++)
 		{
-			_putchar('_');
+			_putchar ('_');
 		}
-		i++;
+		_putchar ('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar ('\n');
+	}
 }
